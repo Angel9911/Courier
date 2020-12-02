46e08db25a66bad075e0a6988dea66b273e8ce79
Angel9911 <angel_dimitrov99@abv.bg>
2020-12-02 22:26:18 +0200

    Update Main.java

--- Courier1/src/home/Main.java
+++ Courier1/src/home/Main.java
@@ -8,16 +8,41 @@ import javafx.stage.Stage;
 
 public class Main extends Application
 {
+    //This is our PrimaryStage (It contains everything)
+    private Stage primaryStage;
 
+    //This is the BorderPane of RootLayout
+    private BorderPane rootLayout;
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("fxml/Main.fxml"));
-        primaryStage.setTitle("AB EXPRESS");
-        primaryStage.setScene(new Scene(root, 765, 465));
-        primaryStage.show();
-        primaryStage.close();
-        }
+        //1) Declare a primary stage (Everything will be on this stage)
+        this.primaryStage = primaryStage;
 
+        //Optional: Set a title for primary stage
+        this.primaryStage.setTitle("SW Test Academy - Sample JavaFX App");
+        try {
+            //First, load EmployeeView from EmployeeView.fxml
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("viewFX/menu_client.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            //Second, show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout); //We are sending rootLayout to the Scene.
+            primaryStage.setScene(scene); //Set the scene in primary stage.
+
+            /*//Give the controller access to the main.
+            RootLayoutController controller = loader.getController();
+            controller.setMain(this);*/
+
+            //Third, show the primary stage
+            primaryStage.show(); //Display the primary stage
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        }
+    /*public void showCientView() {
+
+    }*/
     public static void main(String[] args) {
         launch(args);
     }
