b900884c76f99bba89cf90dcf3a166b1f151fc29
Bozhidar Petrov <chuckbartowski768@gmail.com>
2020-12-02 22:20:26 +0200

    Updata FX

--- Courier1/src/home/Main.java
+++ Courier1/src/home/Main.java
@@ -1,22 +1,33 @@
 package home;
-
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application
-{
+{ Scene scene1;
+
+
+    @FXML Button btn_client_login=new Button();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getResource("fxml/Main.fxml"));
+        Parent root1 = FXMLLoader.load(getClass().getResource("fxml/login_client.fxml"));
+        Parent root2 = FXMLLoader.load(getClass().getResource("fxml/login_admin.fxml"));
+        Parent root3 = FXMLLoader.load(getClass().getResource("fxml/login_courier.fxml"));
+        Parent root4 = FXMLLoader.load(getClass().getResource("fxml/menu_client.fxml"));
+        Parent root5 = FXMLLoader.load(getClass().getResource("fxml/menu_admin.fxml"));
+        Parent root6 = FXMLLoader.load(getClass().getResource("fxml/menu_courier.fxml"));
         primaryStage.setTitle("AB EXPRESS");
         primaryStage.setScene(new Scene(root, 765, 465));
+        primaryStage.setResizable(false);
         primaryStage.show();
-        primaryStage.close();
-        }
+    }
 
     public static void main(String[] args) {
         launch(args);
